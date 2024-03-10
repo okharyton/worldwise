e7a5c2679f933749007effbd0b934531720fb327
Oleg Kharyton <olegkhacv@gmail.com>
2024-03-10 22:03:05 +0200

    feat: add navigation menu enu with content in in

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/AppNav.jsx
@@ -0,0 +1,4 @@
+import { nav } from "./AppNav.module.css";
+export default function AppNav() {
+  return <nav className={nav}>AppNav</nav>;
+}
