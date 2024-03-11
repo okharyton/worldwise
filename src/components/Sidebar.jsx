83048a8d656afceb99b012ffe3ad1aedbbb34fa5
Oleg Kharyton <olegkhacv@gmail.com>
2024-03-11 09:54:21 +0200

    feat: add more куore ready-made components, create Sidebar and Map components for AppLayout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo.jsx";
+import AppNav from "./AppNav.jsx";
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
