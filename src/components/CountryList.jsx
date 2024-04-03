8f25c23fbb3b454d501c18b69a90d54c6bfbb39a
okharyton <olegkhacv@gmail.com>
2024-04-03 22:01:41 +0300

    refactor: add useCities context hook to CityList and CountryList components

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,7 +2,9 @@ import styles from "./CountryList.module.css";
 import Spinner from "./Spinner.jsx";
 import Message from "./Message.jsx";
 import CountryItem from "./CountryItem.jsx";
-export default function CountryList({ cities, isLoading }) {
+import { useCities } from "../contexts/CitiesContext.jsx";
+export default function CountryList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return <Message message="Add your first city by clicking on the map" />;
