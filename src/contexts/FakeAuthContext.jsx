dfa7079ba77e90726b605d41e95aba73ed287eb8
okharyton <olegkhacv@gmail.com>
2024-04-11 00:13:04 +0300

    feat: add fake auth context p2

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,12 +1,51 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { redirect } from "react-router-dom";
 
 const AuthContext = createContext();
 
-function AuthProvider({ children }) {
-  function login(email, password) {}
-  function logout() {}
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
 
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password) {
+      dispatch({ type: "login", payload: FAKE_USER });
+    }
+  }
+  function logout() {
+    dispatch({ type: "logout" });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function useAuth() {
@@ -14,3 +53,5 @@ function useAuth() {
   if (context === undefined)
     throw new Error("AuthContext was used outside the AuthProvider");
 }
+
+export { AuthProvider, useAuth };
