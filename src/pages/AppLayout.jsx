6c8f9f9798fb480b7a3d89e9686aee28fa08dad3
okharyton <olegkhacv@gmail.com>
2024-04-12 00:02:17 +0300

    feat: adiing  fake authentication implementing login

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "../components/Sidebar.jsx";
 import styles from "../pages/AppLayout.module.css";
 import Map from "../components/Map.jsx";
+import User from "../components/User.jsx";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
